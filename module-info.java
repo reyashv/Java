0cb5123529598ca6e851d6d161a2ea786e33cb48
Shreya v <97713189+reyashv@users.noreply.github.com>
2023-10-12 13:09:27 +0530

    Update module-info.java

--- module-info.java
+++ module-info.java
@@ -1,9 +1,10 @@
-module com.example.demo {
+module com.example.jproject {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires com.dlsc.formsfx;
+    requires java.sql;
 
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
+    opens com.example.jproject to javafx.fxml;
+    exports com.example.jproject;
 }
